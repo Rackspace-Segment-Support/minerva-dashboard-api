f395e8e5109f25be6b30c974b30484b68791207a
Kyle Jennings <kjennings.dev@gmail.com>
2019-01-24 10:09:03 -0600

    adds documentation for data access

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# minerva-dashboard-api
+# Minerva Dashboard Api
 Custom Dashboard API for Rackspace Intelligence
+
+## Getting Started
+
+Ensure you have the following dependencies setup:
+
+1. Node & NPM
+2. Docker
+
+To get started simply:
+
+Install dependencies
+
+```
+> npm install
+```
+
+After that you will need to get a MySQL docker instance going.
+We leverage Docker for this so you can just run
+
+```
+> docker-compose -f docker-compose-mysql.yml up -d
+```
+
+with that setup you will be able to start scaffolding the database.
+
+```
+> npm run db:create
+> npm run db:migrate
+> npm run db:sync
+```
+
+Now you will be able to run the application 
+
+```
+> npm start
+```
--- src/models/index.js
+++ src/models/index.js
@@ -1,5 +1,16 @@
 'use strict';
 
+/**
+ * @name index
+ * @description
+ * Main DAL object that encapsulates each model into itself.
+ * The idea here is that accessing models are done through
+ * this module. It also ensures that a proper connection is 
+ * made to the databse based on your current env.
+ * @requires '../../src/config/config.js' config object for DB environments
+ * @requires process.env.NODE_ENV node environemnt object. Maps to DB environments
+ * @returns Parent object for all models
+ */
 const Sequelize = require('sequelize');
 const env = process.env.NODE_ENV || 'development';
 const config = require('../../src/config/config.js')[env];
@@ -16,6 +27,8 @@ db.dashboard = require('../../src/models/dashboard')(conn, Sequelize);
 db.component = require('../../src/models/component')(conn, Sequelize);
 
 // Relationships
+// When two models, defined by 'belongsToMany', point to each other a thrid table is created during a db sync
+// This table is a XREF table for a many-to-many relationship
 db.component.belongsToMany(db.dashboard, { through: 'DashboardComponent', foreignKey: 'componentId', otherKey: 'dashboardId' });
 db.dashboard.belongsToMany(db.component, { through: 'DashboardComponent', foreignKey: 'dashboardId', otherKey: 'componentId' });
 
