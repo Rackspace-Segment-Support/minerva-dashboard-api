b1c6632f2c873cb993030c61fa071a488345dfcc
Kyle Jennings <kjennings.dev@gmail.com>
2019-01-28 14:43:43 -0600

    adds authentication to dashboard API

---
+++ src/middleware/authentication.js
@@ -0,0 +1,39 @@
+const request = require('request-promise');
+
+/**
+ * @name authentication
+ * @description
+ * Middleware for validating authenticated requests to the API
+ */
+module.exports = () => {
+    return (req, res, next) => {
+        var token = req.get('X-Auth-Token');
+
+        // Do we have a token header
+        if (!token) {
+            // If not send 401
+            res.status(401).send('Unauthorized');
+            return;
+        }
+        
+        // Validate the token against idenity
+        request({
+                uri: process.env.IDENTITY_URL + 'tokens/' + token,
+                headers: {
+                    'X-Auth-Token': token
+                }
+            })
+            .then((res) => {
+                // Everything checks out
+                // pass the request to the next
+                // layer of the request chain
+                next();
+            })
+            .catch((err) => {
+                res.status(401).send('Unauthorized');
+                return;
+            });
+
+       
+    };
+};
